5aa1aaef370a8ea62f145fc9776700b1f3cff46a
andrew <adakoleandrew21@gmail.com>
2026-05-23 13:52:54 +0100

    solved leetcode problem 1752

---
+++ LEETCODE/sorted_rotated_array.py
@@ -0,0 +1,16 @@
+def check(array):
+    arr1 = array
+    length = len(arr1)
+    drop_count = 0
+
+    for idx in range(0, length - 1):
+        if arr1[idx] > arr1[idx+1]:
+            drop_count += 1
+
+    if arr1[length -1] > arr1[0]:
+        drop_count += 1
+    
+    return drop_count <= 1
+
+nums = [3,4,5,1,2]
+print(check(nums))
