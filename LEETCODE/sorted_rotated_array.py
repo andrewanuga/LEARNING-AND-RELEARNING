b970f1b0430aff6854198ee00b7088437e6cd081
andrew <adakoleandrew21@gmail.com>
2026-05-23 14:16:56 +0100

    added a comment to the sorted_rotated_array

--- LEETCODE/sorted_rotated_array.py
+++ LEETCODE/sorted_rotated_array.py
@@ -3,6 +3,12 @@ def main():
     return check(nums)
 
 def check(array):
+    # checked for the drop_count if i > i+1 as you loop through the array 
+    # then after the loop check if the end of the array is larger than
+    # the beginning of it if so add to the drop_count 
+    # then if the drop_count is above 1 output false else it is True 
+
+    # The approach tries suggests that the algorithm knows what a rotated array looks like 
     arr1 = array
     length = len(arr1)
     drop_count = 0
@@ -11,10 +17,11 @@ def check(array):
         if arr1[idx] > arr1[idx+1]:
             drop_count += 1
 
-    if arr1[length -1] > arr1[0]:
+    if arr1[length -1] > arr1[0]: # or if arr[-1] > arr1[0]:
         drop_count += 1
     
     return drop_count <= 1
 
 
 print(main())
+
