77db0511d809c011fb5898b04e74dfa423f1bf47
andrew <adakoleandrew21@gmail.com>
2026-06-10 21:37:26 +0100

    algo: close_and_division

--- LEARNING DSA GEEKS OF GEEKS/close_and_divisible.py
+++ LEARNING DSA GEEKS OF GEEKS/close_and_divisible.py
@@ -1,8 +1,8 @@
 def main():
-    m = 13
+    m = 12
     n = 4
 
-    closet_and_divisible_algo = closet_and_divisible(m, n)
+    closet_and_divisible_algo = closet_and_divisible2(m, n)
     print(closet_and_divisible_algo)
 
 def closet_and_divisible(m, n):
@@ -14,6 +14,14 @@ def closet_and_divisible(m, n):
         elif (m + check) % n == 0:
             return m + check
         check += 1
+
+def closet_and_divisible2(m, n):
+    lower_bound = (m // n) * n
+    upper_bound = lower_bound + n
+
+    if ((upper_bound-m) >= (m - lower_bound)):
+        return lower_bound
+    return upper_bound    
 
 if __name__ == '__main__':
     main()
