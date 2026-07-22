c5eec90949f898d1c9225677eda59e35e4a12f98
andrew <adakoleandrew21@gmail.com>
2026-07-23 00:34:54 +0100

    begin and new course on c

---
+++ C/hello.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello World!");
+    // for integers values
+    // printf("%d" , 20)
+
+    // for floating point values
+    // printf("%f", 3.45)
+
+    // for single characters
+    // printf("%c", 'A')
+
+    // for strings
+    // printf("%s", "Hello World!")
+
+    // for octal values
+    // printf("%o", 10)
+
+    // for others
+    // printf("Level %d:", 1);
+
+    // printf("%d%d%c", 10, 1, 'A);
+
+    // for a new line
+    // printf("Name %s\n", "Andrew");
+    // printf("Age %d\n, 20");
+    return 0;
+}
---
+++ C/variable.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int level;
+    // or int level = 100
+    level = 100;
+
+    printf("Level: %d\n", level);
+    return 0;
+}
