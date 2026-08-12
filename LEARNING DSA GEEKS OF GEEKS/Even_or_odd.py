86c9e511539c4a80c7281cf88f3f2c31057671f8
andrew <adakoleandrew21@gmail.com>
2026-08-12 21:02:55 +0100

    made even_or_odd function in the Even_or_Odd.py to a class, then added a stand alone is_evev and is_odd function

--- LEARNING DSA GEEKS OF GEEKS/Even_or_odd.py
+++ LEARNING DSA GEEKS OF GEEKS/Even_or_odd.py
@@ -8,14 +8,20 @@ def main():
     except ValueError:
         return {"error: Add a real number prefferably a Number"}
 
+class check_number_state:
+    def __init__(self, num):
+        self.num = num
+    def even_or_odd(self):
+        if self.num % 2 == 0:
+            return "Even"
+        return "Odd"
 
-def even_or_odd(number):
-    if number % 2 == 0:
-        return "Even"
-    return "Odd"
+    def is_even(self):
+        return 1 if self.num % 2 == 0 else 0
+
+    def is_odd(self):
+        return 1 if self.num % 2 != 0 else 0
+
+
 
 print(main())
-
-
-def is_even(num):
-    return 1 if num % 2 == 0 else 0
